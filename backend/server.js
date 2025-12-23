540be4126586ebf9b799a80201a5474774d47988
aarsh <144159115+Aarshpatel12@users.noreply.github.com>
2025-12-23 16:33:33 +0530

    Mera pehla commit

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Database Connection
+// ✅ RIGHT
+// Notice karo: Maine < > hata diye hain aur password daal diya hai.
+// Maine '/' ke baad 'mern-todo' bhi add kiya hai taaki data sahi folder (DB) mein save ho.
+
+const dbURI = "mongodb+srv://admin:fByKTQcGKXT8KT7O@taskmanager.gyltg0u.mongodb.net/?appName=taskmanager";
+
+mongoose.connect(dbURI)
+    .then(() => console.log("Connected to MongoDB Atlas!"))
+    .catch((err) => console.log("Error connecting to Atlas:", err));
+
+// Schema Definition
+const TaskSchema = new mongoose.Schema({
+    todo: String
+});
+
+const TaskModel = mongoose.model("tasks", TaskSchema);
+
+// API Routes
+
+// 1. Data Save karne ke liye (Create)
+app.post('/add', (req, res) => {
+    const task = req.body.task;
+    TaskModel.create({ todo: task })
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+// 2. Data Lene ke liye (Read)
+app.get('/get', (req, res) => {
+    TaskModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+// Server Start
+app.listen(3001, () => {
+    console.log("Server is Running on port 3001");
+});
